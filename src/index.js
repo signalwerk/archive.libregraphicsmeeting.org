7974b7fa815c96a70533c5a3bf5de770b55dc9cb
signalwerk <sh@signalwerk.ch>
2025-05-02 19:21:55 +0200

    MOD: update scraper

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
-import { WebServer } from "./server/server.js";
-import { Cache } from "./server/utils/Cache.js";
-import { RequestTracker } from "./server/utils/RequestTracker.js";
-import { DataPatcher } from "./server/utils/DataPatcher.js";
+import { WebServer } from "../packages/scrape-helpers/src/server/server.js";
+import { getRelativeURL } from "../packages/scrape-helpers/src/server/utils/getRelativeURL.js";
+import { DataPatcher } from "../packages/scrape-helpers/src/server/utils/DataPatcher.js";
+import { removeCommentsIf } from "../packages/scrape-helpers/src/server/processor/cheerio-helper.js";
 
-import { isDomainValid, isAlreadyRequested } from "./server/processor/request.js";
+import {
+  isDomainValid,
+  isPathValid,
+  isAlreadyRequested,
+} from "../packages/scrape-helpers/src/server/processor/request.js";
 import {
   addParseJob,
   guessMimeType,
-  parseHtml,
-  parseCss,
-} from "./server/processor/parse.js";
-import { addFetchJob } from "./server/processor/fetch.js";
-import { isCached, fetchHttp } from "./server/processor/fetch.js";
+  parseFiles,
+} from "../packages/scrape-helpers/src/server/processor/parse.js";
+import { addFetchJob } from "../packages/scrape-helpers/src/server/processor/fetch.js";
+import {
+  isCached,
+  fetchHttp,
+} from "../packages/scrape-helpers/src/server/processor/fetch.js";
+import {
+  handleRedirected,
+  writeOutput,
+  isAlreadyWritten,
+} from "../packages/scrape-helpers/src/server/processor/write.js";
 
-// Create instances of required components
-const cache = new Cache();
-const requestTracker = new RequestTracker();
 const dataPatcher = new DataPatcher();
 
 // Configure data patcher rules
+// change content that is different for each request
+
 dataPatcher
   .addRule({
     includes: [
@@ -52,158 +62,54 @@ dataPatcher
     ],
     search: "W -webkit-box-shadow:2px 2px 4px rgba(0,0,0,0.25);",
     replace: "-webkit-box-shadow:2px 2px 4px rgba(0,0,0,0.25);",
+  })
+  .addRule({
+    includes: [
+      //
+      "https://www.libregraphicsmeeting.org/2007/global.css",
+    ],
+    search: "color}",
+    replace: "}",
   });
 
 // Create server instance
 const server = new WebServer({
-  cache,
+  urls: ["https://libregraphicsmeeting.org/"],
   dataPatcher,
-  requestTracker,
-  requestConcurrency: 100,
-  fetchConcurrency: 10,
-  parseConcurrency: 100,
 });
 
 // Configure queue processors
 server.configureQueues({
   request: [
-    async (job, next) =>
-      await isDomainValid(
-        {
-          job,
-          allowed: [/^([a-z0-9-]+\.)*libregraphicsmeeting\.org$/i],
-        },
-        next,
-      ),
-    async (job, next) =>
-      await isAlreadyRequested(
-        {
-          job,
-          requestTracker,
-          getKey: (job) => job.data.uri,
-        },
-        next,
-      ),
-    async (job, next) =>
-      await addFetchJob(
-        {
-          job,
-          events: server.events,
-        },
-        next,
-      ),
+    isDomainValid({
+      allowed: [/^([a-z0-9-]+\.)*libregraphicsmeeting\.org$/i],
+    }),
+    // isPathValid({
+    //   disallowed: [
+    //     /.*(Diskussion|action=|Spezial|Benutzer.*oldid|Hauptseite.*oldid|title=.*oldid|printable=yes).*/i,
+    //   ],
+    // }),
+    isAlreadyRequested(),
+    addFetchJob(),
   ],
-  fetch: [
-    async (job, next) =>
-      await isCached(
-        {
-          job,
-          events: server.events,
-          cache,
-          getKey: (job) => job.data.uri,
+  fetch: [isCached(), fetchHttp(), addParseJob()],
+  parse: [guessMimeType(), parseFiles()],
+  write: [
+    isAlreadyWritten(),
+    handleRedirected(),
+    guessMimeType(),
+    writeOutput({
+      getUrl: ({ absoluteUrl, baseUrl }) =>
+        getRelativeURL(absoluteUrl, baseUrl, true, false, true),
+      rewrite: {
+        ["text/html"]: ($) => {
+          // This function removes old IE conditional comments from the HTML.
+          // removeCommentsIf($, {
+          //   includes: ["[if", "endif]"],
+          // });
         },
-        next,
-      ),
-    async (job, next) =>
-      await fetchHttp(
-        {
-          job,
-          cache,
-          events: server.events,
-        },
-        next,
-      ),
-    async (job, next) =>
-      await addParseJob(
-        {
-          job,
-          events: server.events,
-        },
-        next,
-      ),
-  ],
-  parse: [
-    async (job, next) =>
-      await guessMimeType(
-        {
-          job,
-          cache,
-        },
-        next,
-      ),
-    async (job, next) => {
-      const { data: dataFromCache, metadata } = cache.get(job.data.cache.key);
-
-      const data = dataPatcher.patch(job.data.uri, `${dataFromCache}`, (log) =>
-        job.log(log),
-      );
-
-      if (!data || !metadata) {
-        throw new Error(
-          `No data or metadata found in cache ${job.data.cache.key}`,
-        );
-      }
-
-      const mimeType = job.data.mimeType;
-
-      switch (mimeType) {
-        case "application/xhtml+xml":
-        case "text/html": {
-          await parseHtml({ job, events: server.events, data }, next);
-          break;
-        }
-        case "text/css": {
-          await parseCss({ job, events: server.events, data }, next);
-          break;
-        }
-        case "application/javascript":
-        //
-        case "text/plain":
-        case "image/png":
-        case "image/jpeg":
-        case "image/jpg":
-        case "image/gif":
-        case "image/webp":
-        case "image/svg+xml":
-        case "image/avif":
-        case "image/apng":
-        case "image/bmp":
-        case "image/tiff":
-        case "image/x-icon":
-        case "text/xml":
-        case "image/vnd.microsoft.icon":
-        case "application/vnd.oasis.opendocument.text":
-        case "application/pdf":
-        case "application/json":
-        case "application/x-font-ttf":
-        case "font/ttf":
-        case "font/woff":
-        case "font/woff2":
-        case "application/vnd.ms-fontobject": // eot
-        case "application/rss+xml":
-        case "application/atom+xml":
-        case "application/rdf+xml":
-        case "application/rss+xml":
-        case "application/rdf+xml":
-        case "application/x-rss+xml":
-        case "application/xml":
-        case "application/x-www-form-urlencoded":
-        case "application/x-shockwave-flash":
-        case "application/epub+zip": {
-          //
-          break;
-        }
-        default: {
-          throw new Error(
-            `Unsupported content type: ${
-              metadata.headers["content-type"] || "undefined"
-            }`,
-          );
-        }
-      }
-
-      next();
-    },
+      },
+    }),
   ],
 });
 
